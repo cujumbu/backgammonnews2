c9f541363a0d2f73a51b60585003b1faaf070c9f
cujumbu <nethandelskompagniet@gmail.com>
2024-11-16 21:29:16 +0100

    Update index.ts

--- app/tools/position-analyzer/lib/index.ts
+++ app/tools/position-analyzer/lib/index.ts
@@ -1,3 +1,3 @@
-export type { Position, PositionAnalysis } from './types';
-export { parsePosition } from './parser';
-export { analyzePosition } from './analyzer';
+export * from './types';
+export * from './parser';
+export * from './analyzer';
