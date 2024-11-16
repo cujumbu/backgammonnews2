28f67c08e31694822ea2cc11cc6b0c4a180a1cb3
cujumbu <nethandelskompagniet@gmail.com>
2024-11-16 22:18:28 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,13 +9,13 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        'point-dark': '#8B4513',
+        'point-dark': '#654321',
         'point-light': '#DEB887',
         'wood-dark': '#5C4033',
       },
-      backgroundImage: {
-        'wood-pattern': "url('data:image/svg+xml,%3Csvg width=\"100\" height=\"100\" viewBox=\"0 0 100 100\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cfilter id=\"noise\"%3E%3CfeTurbulence type=\"fractalNoise\" baseFrequency=\"0.15\" numOctaves=\"3\" stitchTiles=\"stitch\"/%3E%3C/filter%3E%3Crect width=\"100\" height=\"100\" filter=\"url(%23noise)\" opacity=\"0.4\"/%3E%3C/svg%3E'), linear-gradient(to right, #8B4513, #A0522D)",
-      },
+      zIndex: {
+        '100': '100',
+      }
     },
   },
   plugins: [],
