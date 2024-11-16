ce3e1d9058d9040a853f1ca937334d6771173e00
cujumbu <nethandelskompagniet@gmail.com>
2024-11-16 17:48:59 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 import NewsGrid from '@/components/news-grid';
 
-export default function NewsPage() {
+export default function Home() {
   return (
     <div>
-      <h1 className="text-2xl font-bold mb-6">Latest Backgammon News</h1>
-      <NewsGrid limit={20} />
+      <h1 className="text-2xl font-bold mb-6">BackgammonNews</h1>
+      <NewsGrid />
     </div>
   );
 }
